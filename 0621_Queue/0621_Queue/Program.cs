d269295be3bedcb6aad1d81c04c39de9422ae4ae
IDAlearning <37046677+IDAlreaning@users.noreply.github.com>
2018-06-21 21:39:09 +0800

    0621_add
    Add exercise bata of stack and exercise bata of queue.

---
+++ 0621_Queue/0621_Queue/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0621_Queue
+{
+    class Program
+    {
+        public static void PrintValues(IEnumerable myCollection)
+        {
+            foreach (string item in myCollection)
+            {
+                Console.Write("{0} ", item);
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            Queue<string> myQueue = new Queue<string>();
+            myQueue.Enqueue("5");
+            myQueue.Enqueue("4");
+            myQueue.Enqueue("3");
+            myQueue.Enqueue("2");
+            myQueue.Enqueue("1");
+
+            PrintValues(myQueue);
+            Console.WriteLine("=====");
+            Console.WriteLine("Count: " + myQueue.Count);
+
+            Console.WriteLine("=====");
+            Console.WriteLine("Peek: " + myQueue.Peek());
+
+            Console.WriteLine("=====");
+            Console.WriteLine("Pop: " + myQueue.Dequeue());
+
+            Console.WriteLine("=====");
+            PrintValues(myQueue);
+
+            Console.WriteLine("=====");
+            Console.WriteLine(myQueue.ElementAt(1));
+
+            Console.WriteLine("==CopyTo==");
+            Queue<string> queueCopy = new Queue<string>(myQueue.ToArray());
+
+            PrintValues(queueCopy);
+
+            Console.WriteLine("queueCopy.Contains(\"5\") = {0}", queueCopy.Contains("5"));
+            queueCopy.Clear();
+            Console.WriteLine("Clear, so queueCopy.Count = {0}", queueCopy.Count);
+        }
+    }
+}
---
+++ 0621_stack/0621_stack/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0621_stack
+{
+    class Program
+    {
+        public static void PrintValues(IEnumerable myCollection)
+        {
+            foreach (string obj in myCollection)
+            {
+                Console.Write("{0} ", obj);
+            }
+            Console.WriteLine();
+            Console.WriteLine("========");
+        }
+        static void Main(string[] args)
+        {
+            Stack<string> myStack = new Stack<string>();
+            myStack.Push("5");
+            myStack.Push("4");
+            myStack.Push("3");
+            myStack.Push("2");
+            myStack.Push("1");
+
+            Console.WriteLine("Count:{0}", myStack.Count);
+            PrintValues(myStack);
+
+
+            Console.WriteLine("Peek: "+myStack.Peek());
+
+            Console.WriteLine("========");
+            Console.WriteLine("Pop: " + myStack.Pop());
+
+            Console.WriteLine("========");
+            Console.WriteLine("剩下的: " );
+            PrintValues(myStack);
+
+
+            Stack<string> Stack2 = new Stack<string>(myStack.ToArray());
+            PrintValues(Stack2);
+
+
+        }
+    }
+}
